01087795b9feb7cd891138a85bc8bad0e331ebaf
Raimo <>
2019-03-08 22:01:30 +0200

    removed deploy script

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-# docker tag local-image:tagname reponame:tagname
-# docker push reponame:tagname
-cat ../docker_password.txt | docker login -u raimojohanson --password-stdin
-docker build -t vet-api .
-docker tag vet-api raimojohanson/vet-api
-docker push raimojohanson/vet-api
